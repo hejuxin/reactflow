01069633957245cd9f08a91d6db056dee2097eb7
hejuxin <xiangjian1@supcon.com>
2024-09-05 14:59:41 +0800

    feat: 子泳道拖拽缩放功能优化调整

--- src/nodes/Swim/LaneNode.jsx
+++ src/nodes/Swim/LaneNode.jsx
@@ -4,6 +4,10 @@ import cn from 'classnames';
 import { laneHeight, laneMinHeight, titleWidth } from '@/utils/swim';
 import './index.less';
 
+const getHeight = node => {
+  return node.height ?? node.measured.height;
+}
+
 const LaneNode = (props) => {
   const { selected = false, id, parentId } = props;
   const [maxHeight, setMaxHeight] = useState();
@@ -18,13 +22,12 @@ const LaneNode = (props) => {
 
   const isTopRef = useRef();
   const isLeftRef = useRef();
-  // const handleResize = e => {
-  //   console.log(e, 'handleResize')
-  // }
+  
   const handleResizeEnd = e => {
     const node = nodes.find(node => node.id === id);
 
-    const { width, height } = node.measured;
+    const width = node.width ?? node.measured.width;
+    const height = getHeight(node);
     const { width: startWidth, height: startHeight } = startSize.current;
 
     if (startWidth !== width) {
@@ -71,26 +74,19 @@ const LaneNode = (props) => {
       const diffH = height - startHeight;
       // 第一个子节点，且拖拽的是上边框
       if (isFirstNode && isTopRef.current) {
-        console.log(diffH)
         reactflow.updateNode(parentId, (node) => {
-          node.height = node.measured.height + diffH;
-          const position = node.position;
-          console.log(position, 'parentId')
-          node.position = {
-            ...position,
-            y: position.y - diffH
-          }
+          const height = getHeight(node);
+          node.height = height + diffH;
+
+          const y = node.position.y;
+          node.position.y = y - diffH;
           return { ...node }
         })
 
         laneNodes.forEach(node => {
           reactflow.updateNode(node.id, (node) => {
-            const position = node.position;
-            console.log(position, 'laneNodes')
-            node.position = {
-              ...position,
-              y: position.y + diffH
-            }
+            const y = node.position.y;
+            node.position.y = y + diffH;
             return { ...node }
           })
         })
@@ -101,7 +97,8 @@ const LaneNode = (props) => {
       // 最后一个子节点，且拖拽的是下边框
       if (isLastNode && !isTopRef.current) {
         reactflow.updateNode(parentId, (node) => {
-          node.height = node.measured.height + diffH;
+          const height = getHeight(node);
+          node.height = height + diffH;
           return { ...node }
         })
 
@@ -117,10 +114,8 @@ const LaneNode = (props) => {
 
       if (isTopRef.current) {
         const needChangeNode = nodes[index - 1];
-        console.log(needChangeNode, 'isTopRef.current')
         reactflow.updateNode(needChangeNode.id, (node) => {
-          const currentHeight = node.height ?? node.style.height;
-          console.log(currentHeight, diffH)
+          const currentHeight = getHeight(node);
           node.height = currentHeight - diffH;
           return { ...node }
         })
@@ -128,31 +123,15 @@ const LaneNode = (props) => {
       } else {
         const needChangeNode = nodes[index + 1];
         reactflow.updateNode(needChangeNode.id, (node) => {
-          const currentHeight = node.height ?? node.style.height;
+          const currentHeight = getHeight(node);
           node.height = currentHeight - diffH;
-          const position = node.position;
-          node.position = {
-            ...position,
-            y: position.y + diffH
-          }
+          
+          const y = node.position.y;
+          node.position.y = y + diffH;
           return { ...node }
         })
       }
     }
-
-    // if (startHeight < height) {
-    //   reactflow.updateNode(parentId, (node) => {
-    //     node.height = node.height - startHeight + height
-    //     return { ...node }
-    //   })
-    // } else {
-    //   console.log('height change')
-    //   reactflow.updateNode(parentId, (node) => {
-    //     console.log(node.height, )
-    //     node.height = node.height - startHeight + height
-    //     return { ...node }
-    //   })
-    // }
   }
 
   const handleResizeStart = e => {
@@ -170,22 +149,15 @@ const LaneNode = (props) => {
 
     if (isTop) {
       if (isFirstNode) return;
-
       const needChangeNode = nodes[index - 1];
-      console.log(needChangeNode, 'isTop')
-      const diffH = (needChangeNode.height ?? needChangeNode.measured.height) - laneMinHeight;
-      console.log(diffH, 'diffH')
-      const currentNodeMaxHeight = (node.height ?? node.measured.height) + diffH;
-      console.log(currentNodeMaxHeight, 'currentNodeMaxHeight')
+      const diffH = getHeight(needChangeNode) - laneMinHeight;
+      const currentNodeMaxHeight = getHeight(node) + diffH;
       setMaxHeight(() => currentNodeMaxHeight)
     } else {
       if (isLastNode) return;
       const needChangeNode = nodes[index + 1];
-      console.log(needChangeNode, 'not isTop')
-      const diffH = (needChangeNode.height ?? needChangeNode.measured.height) - laneMinHeight;
-      console.log(diffH, 'diffH')
-      const currentNodeMaxHeight = (node.height ?? node.measured.height) + diffH;
-      console.log(currentNodeMaxHeight, 'currentNodeMaxHeight')
+      const diffH = getHeight(needChangeNode) - laneMinHeight;
+      const currentNodeMaxHeight = getHeight(node) + diffH;
       setMaxHeight(() => currentNodeMaxHeight)
     }
   }
@@ -226,11 +198,8 @@ const LaneNode = (props) => {
         onResizeStart={handleResizeStart}
         onResizeEnd={handleResizeEnd}
         {...resizerprops}
-      // lineStyle={{
-      //   display: 'none'
-      // }}
       />
-      {props.data.label}
+      {props.data?.label}
     </div>
   )
 }
